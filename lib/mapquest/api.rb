51629e88b314d3a6d566c0c734ef8894082d292a
Chase Clettenberg <clettenberg@gmail.com>
2018-08-28 07:52:04 -0500

    Initial Commit

---
+++ lib/mapquest/api.rb
@@ -0,0 +1,7 @@
+require "mapquest/api/version"
+
+module Mapquest
+  module Api
+    # Your code goes here...
+  end
+end
---
+++ spec/mapquest/api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Mapquest::Api do
+  it "has a version number" do
+    expect(Mapquest::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
